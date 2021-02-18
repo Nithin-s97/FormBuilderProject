b768d87c3df44308319c018d39eb4babca25ec6a
Nithin-s97 <79255895+Nithin-s97@users.noreply.github.com>
2021-02-18 17:42:51 +0530

    Add files via upload

---
+++ Form-Builder/form-builder/backend/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+// require('dotenv').config();
+
+const app = express(); 
+const port = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb+srv://nithin_s:nith3159@cluster0.wnjby.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+})
+.then(() => {
+    console.log('MongoDb connected..........');
+})
+// const uri = process.env.ATLAS_URI;
+// mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+// );
+// const connection = mongoose.connection;
+// connection.once('open', () => {
+//   console.log("MongoDB database connection established successfully");
+// })
+
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
